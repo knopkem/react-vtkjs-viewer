2a554ae310f00013ecfb4c3334e1134cf0b4dbb7
Michael Knopke <knopkem@gmail.com>
2020-08-21 13:43:22 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import VTKLoadImageDataExample from './VTKLoadImageDataExample.js';
+import VTKMPRRotateExample from './VTKMPRRotateExample.js';
+import VTKVolumeRenderingExample from './VTKVolumeRenderingExample.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <VTKLoadImageDataExample />
+      </header>
+    </div>
+  );
+}
+
+export default App;
